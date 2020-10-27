ffb543809201eacc9fd9ed9bdcf943bdfd94f0a8
orxanmuv <orxanmusayev1102@gmail.com>
2020-10-27 17:38:00 +0400

    Initial repositories created

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -10,8 +11,15 @@ namespace Infrastructure.Data
     public class StoreContext : DbContext
     {
         public DbSet<Product> Products{ get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
